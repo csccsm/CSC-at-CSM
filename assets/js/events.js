3ba78b23b69006b182d4f74fcb2e53e841d4fa3a
Christian Lapinig <christianlapinig@Christians-MacBook-Pro.local>
2016-01-02 16:43:53 -0800

    List events on events page

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,4 +1,4 @@
-Parse.initialize("pls2KT1xZhiJJ99gJL5FQ7DyxVgq4iN1s6yxw1Kh", "BPYMCRgdjQ0oTTCNvrKZRIq4GOuSqOploiW7pT4r");
+Parse.initialize("XXXXX", "XXXXX");
 
 var Events = Parse.Object.extend("Events");
 
@@ -21,3 +21,25 @@ $("#event-form").submit(function(event) {
       }
    });
 });
+
+function getEvents() {
+   var query = new Parse.Query(Events);
+   query.find({
+      success: function(event) {
+         var output = "";
+         for (var i in event) {
+            var title = event[i].get("title");
+            var description = event[i].get("description");
+            output += "<li>";
+            output += "<h3>"+title+"</h3>";
+            output += "<p>"+description+"</p>"
+            output += "</li>";
+         }
+         $("#list-events").html(output);
+      },
+      error: function(err) {
+         console.log("Error: " + err.message);
+      }
+   });
+}
+getEvents();
