6640ce9694732a1b8685c49f269112baa7964a41
Christian Lapinig <christianlapinig@Christians-MacBook-Pro.local>
2016-01-03 23:33:34 -0800

    Correction to routes

--- routes.js
+++ routes.js
@@ -1,35 +1,33 @@
 var path = require('path');
-var bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
 var express = require('express');
 var router = express.Router();
 
 module.exports = function(app) {
   router.use('/assets', express.static(__dirname + '/assets'));
-  router.use(express.static(__dirname + '/views'))
+  router.use(express.static(__dirname + '/views'));
 
   router.get('/', function(req, res) {
-    res.send(path.join(__dirname + '/views/index.html'));
+    res.send(path.join(__dirname + '/views/index'));
   });
 
   router.get('/events', function(req, res) {
-    res.send(path.join(__dirname + '/views/events.html'));
+    res.send(path.join(__dirname + '/views/events'));
   });
 
   router.get('/about', function(req, res) {
-    res.send(path.join(__dirname + '/views/about.html'));
+    res.send(path.join(__dirname + '/views/about'));
   });
 
   router.get('/contact', function(req, res) {
-    res.send(path.join(__dirname + '/views/contact.html'));
+    res.send(path.join(__dirname + '/views/contact'));
   });
 
   router.get('/members', function(req, res) {
-    res.send(path.join(__dirname + '/views/members.html'));
+    res.send(path.join(__dirname + '/views/members'));
   });
 
   router.get('/projects', function(req, res) {
-    res.send(path.join(__dirname + '/views/projects.html'));
+    res.send(path.join(__dirname + '/views/projects'));
   });
   return router;
 }
