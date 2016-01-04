d5d5fed291778e168da8462579ed00aceb44f5a3
Christian Lapinig <christianlapinig@Christians-MacBook-Pro.local>
2016-01-03 18:46:08 -0800

    Configure routes

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
-var path = require('path');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
 var express = require('express');
 var app = express();
 
 var ip = process.env.IP || '127.0.0.1' || 'localhost';
 var port = process.env.PORT || 8080;
 
-app.use('/assets', express.static(__dirname + '/assets'));
+app.use(logger('dev'));
+app.use(cookieParser());
+app.use(bodyParser());
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/views/index.html'));
-});
+// Configure routes=============================================================
+app.use('/', require('./routes')(app));
 
-app.get('/events', function(req, res) {
-  res.sendFile(path.join(__dirname + '/views/events.html'));
-});
-
-app.get('/about', function(req, res) {
-  res.sendFile(path.join(__dirname + '/views/about.html'));
-});
-
-app.get('/contact', function(req, res) {
-  res.sendFile(path.join(__dirname + '/views/contact.html'));
-});
-
-app.get('/members', function(req, res) {
-  res.sendFile(path.join(__dirname + '/views/members.html'));
-});
-
-app.get('/projects', function(req, res) {
-  res.sendFile(path.join(__dirname + '/views/projects.html'));
-});
 // Launch server================================================================
 app.listen(port);
 console.log('%s:Server starting at %s:%d', Date(Date.now()), ip, port);
