4572681b3c2cb3db5d89d0db7fa91c77395ada21
Christian Lapinig <clapinig93@gmail.com>
2016-05-12 21:35:15 -0700

    Handle errors and/or if page is not found

--- routes/events.js
+++ routes/events.js
@@ -5,6 +5,9 @@ let Event = require('../schemas/event');
 let router = express.Router();
 
 module.exports = function(app) {
+  /*
+  The HTTP methods GET and POST are used. The GET method retrieves the data from the events collection in our MongoDB db. The POST method will send that data to the events collection to be saved in the db.
+  */
   // Retrieve all events to show on page========================================
   router.get('/', function(req, res) {
     Event.find({}, function(err, docs) {
@@ -17,12 +20,13 @@ module.exports = function(app) {
 
   // Save new events============================================================
   router.post('/', function(req, res) {
+    // Create a new Event object to be saved in the db
     let newEvent = new Event();
-
     newEvent.title = req.body.title;
     newEvent.date = req.body.date;
     newEvent.description = req.body.description;
 
+    // Save new Event
     newEvent.save(function(err) {
       if (err) {
         res.sendStatus(403);
--- routes/projects.js
+++ routes/projects.js
@@ -5,6 +5,10 @@ let Project = require('../schemas/project');
 let router = express.Router();
 
 module.exports = function(app) {
+	/*
+  The HTTP methods GET and POST are used. The GET method retrieves the data from the events collection in our MongoDB db. The POST method will send that data to the projects collection to be saved in the db.
+  */
+	// Retrieve all projectss to show on page=====================================
 	router.get('/', function(req, res) {
   	Project.find({}, function(err, docs) {
 			if (err) {
@@ -14,13 +18,15 @@ module.exports = function(app) {
 		});
   });
 
+	// Save new projects==========================================================
 	router.post('/', function(req, res) {
+		// Create new Project object
 		let newProject = new Project();
-
 		newProject.title = req.body.title;
 		newProject.link = req.body.link;
 		newProject.description = req.body.description;
 
+		// Save new Project
 		newProject.save(function(err) {
 			if (err) {
 				res.sendStatus(403);
